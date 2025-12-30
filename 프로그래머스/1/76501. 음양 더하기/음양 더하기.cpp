ea1b01c91786365a9bf80d6c4fc5cc8fc1fbb415
Bowon Jeong/정보원 <150906466+bowons@users.noreply.github.com>
2025-12-30 09:02:55 +0900

    [level 1] Title: 음양 더하기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    
+    for (int i = 0; i < absolutes.size(); i++)
+    {
+        int sign = signs[i] ? +1 : -1;
+        answer += absolutes[i] * sign;
+    }
+    
+    return answer;
+}
