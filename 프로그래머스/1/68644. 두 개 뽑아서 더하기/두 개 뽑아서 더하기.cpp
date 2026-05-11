05576f0aebe927454512054904af39995114f3dd
bowons <150906466+bowons@users.noreply.github.com>
2026-05-12 06:21:00 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.19 ms, Memory: 4.09 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cpp
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cpp
@@ -5,16 +5,22 @@
 using namespace std;
 
 vector<int> solution(vector<int> numbers) {
-    set<int> s;
     
-    for (int i = 0; i < numbers.size(); i++)
+    // 1. 배열의 모든 수를 순회하면서 더하여 나온 값을 계속해서 저장한다
+    // 2. 여기서 중복 값을 제거하면 됨
+    
+    // set을 이용하면 된다
+    set<int> sum;
+    
+    for (int i = 0; i < numbers.size(); i++) 
     {
-        for (int j = i + 1; j < numbers.size(); j++)
+        for (int j = i + 1; j < numbers.size(); j++) 
         {
-            s.insert(numbers[i] + numbers[j]);
+            sum.insert(numbers[i] + numbers[j]);
         }
     }
     
-    vector<int> answer(s.begin(), s.end());
+    vector<int> answer(sum.begin(), sum.end());
+    
     return answer;
 }
