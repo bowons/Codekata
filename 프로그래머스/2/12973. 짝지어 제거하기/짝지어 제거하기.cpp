388a577a602421bf4f38462e2d1746e15d04ee4e
bowons <150906466+bowons@users.noreply.github.com>
2026-06-10 01:22:01 +0900

    [level 2] Title: 짝지어 제거하기, Time: 4.41 ms, Memory: 6.09 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -1,21 +1,13 @@
 #include <iostream>
-#include <string>
-#include <stack>
-
+#include<string>
 using namespace std;
 
 int solution(string s)
 {
-    stack<char> stk;
-    
-    for (int i = 0; i < s.length(); i++)    
-    {
-        // 스택이 비어 있거나, 현재 문자와 같은 문자가 아니면 푸쉬
-        if (stk.empty() || stk.top() != s[i])
-            stk.push(s[i]);
-        else // 현재 문자와 가장 최근 문자가 같다면 팝
-            stk.pop();
-    }
-    
-    return stk.empty(); // 비었다면 짝이 맞는 것임
+    int answer = -1;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
+    return answer;
 }
