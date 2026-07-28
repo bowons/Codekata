a16abfe24ce52902ac8b29ca22708a435ae9f88c
bowons <150906466+bowons@users.noreply.github.com>
2026-07-28 15:01:18 +0900

    [Passed] 세 실수의 반올림 (2ms, 0MB)

---
+++ trail0/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float a = 0, b = 0, c = 0;
+
+    cin >> a >> b >> c;
+
+    printf("%.3f\n", a);
+    printf("%.3f\n", b);
+    printf("%.3f\n", c);
+
+    return 0;
+}
