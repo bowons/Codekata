8702f6c364c2154acb7afcc09fda7240032c5bdb
bowons <150906466+bowons@users.noreply.github.com>
2026-07-28 12:35:56 +0900

    [Passed] 변수 출력하기 3 (68ms, 24MB)

---
+++ trail0/변수 출력하기 3/outputing-variables-3.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int a = 1, b = 2;
+        char c = 'C';
+
+        Console.WriteLine("{0}->{1}->{2}", a, b, c);
+    }
+}
