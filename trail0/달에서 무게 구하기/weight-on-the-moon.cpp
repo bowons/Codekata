fbe5a6877a2018b2dd4f4f5e8de6ab13662de400
bowons <150906466+bowons@users.noreply.github.com>
2026-07-28 12:49:04 +0900

    [Passed] 달에서 무게 구하기 (2ms, 0MB)

---
+++ trail0/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int weight = 13;
+    float gravity = 0.165f;
+
+    printf("%d * %.6f = %.6f", weight, gravity, (weight * gravity));
+    return 0;
+}
