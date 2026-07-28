50f6f9a8aa2a7aae71c32a1c26f767d1fc51f974
bowons <150906466+bowons@users.noreply.github.com>
2026-07-28 15:06:11 +0900

    [Passed] 합과 평균 (2ms, 0MB)

---
+++ trail0/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int A, B;
+
+    cin >> A >> B;
+
+    printf("%d %.1f", (A + B), (float)(A + B) / 2.0f);
+    return 0;
+}
