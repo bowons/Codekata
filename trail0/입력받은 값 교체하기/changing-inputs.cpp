30980ad1fd51553e0718a10b8c9b5b371e0b5128
bowons <150906466+bowons@users.noreply.github.com>
2026-07-28 15:01:57 +0900

    [Passed] 입력받은 값 교체하기 (2ms, 0MB)

---
+++ trail0/입력받은 값 교체하기/changing-inputs.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a, b;
+
+    cin >> a >> b;
+
+    printf("%d %d", b , a);
+    return 0;
+}
