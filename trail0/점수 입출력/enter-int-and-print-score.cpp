fa5c39f0fa2e0dc1464393e5c1bd2049086b64fd
bowons <150906466+bowons@users.noreply.github.com>
2026-07-28 12:55:25 +0900

    [Passed] 점수 입출력 (2ms, 0MB)

---
+++ trail0/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N;
+    cin >> N;
+
+    //cout << "Your score is " << N << " point.";
+    printf("Your score is %d point.", N);
+
+    return 0;
+}
