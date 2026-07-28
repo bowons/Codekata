e4291e8b8457a59cf8e4afe7b067bbe38250f2f8
bowons <150906466+bowons@users.noreply.github.com>
2026-07-28 15:02:31 +0900

    [Passed] 입력받은 값과 합 출력 (2ms, 0MB)

---
+++ trail0/입력받은 값과 합 출력/output-of-value-and-sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int A, B;
+
+    cin >> A >> B;
+
+    printf("%d %d %d", A, B, (A+B));
+    return 0;
+}
