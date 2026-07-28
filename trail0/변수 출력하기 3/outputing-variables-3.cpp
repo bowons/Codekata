c83a300d611a41abe526a96ba001ae35f9697e0e
bowons <150906466+bowons@users.noreply.github.com>
2026-07-28 12:36:35 +0900

    [Passed] 변수 출력하기 3 (2ms, 0MB)

---
+++ trail0/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char a = '0' + 1, b = '0' + 2, c = 'C';
+
+    cout << a << "->" << b << "->" << c;
+    
+    return 0;
+}
