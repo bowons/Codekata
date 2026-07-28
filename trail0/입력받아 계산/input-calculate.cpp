92107defbd66a99943d9fd1d767826c358787921
bowons <150906466+bowons@users.noreply.github.com>
2026-07-28 13:59:25 +0900

    [Passed] 입력받아 계산 (2ms, 0MB)

---
+++ trail0/입력받아 계산/input-calculate.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+
+    cin >> a; 
+    a += 2;
+
+    printf("%d", a);
+    
+    return 0;
+}
