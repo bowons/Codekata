578f9d012d32e4facd64aab970505992c7369556
bowons <150906466+bowons@users.noreply.github.com>
2026-08-05 12:29:30 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,45 +1,37 @@
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
+    int n = progresses.size();
     
-    // 1. 각 기능이 완료되기 까지 필요한 날짜 계산
-    // day = (100 - progress + speed - 1) / speed
-    vector<int> days;
+    vector<int> days_left(n);
     
-    for (int i = 0; i < progresses.size(); i++)
+    for (int i = 0; i < n; i++)
     {
-        int remain = 100 - progresses[i];
-        int day = (remain + speeds[i] - 1) / speeds[i];
-        
-        days.push_back(day);
+        days_left[i] = ceil((100.0 - progresses[i]) / speeds[i]);
     }
     
-    // 2. 첫 번째 기능의 완료일을 기준일로 설정
-    int standard = days[0];
-    int count = 1;
+    int count = 0;
+    int max_day = days_left[0];
     
-        // 3, 다음 기능들 순회
-    // 완료일 <= 기준일 -> 같은 배포 그룹 Count 증가
-    // 완료일 > 기준일 -> 새로운 배포 그룹 Count를 Answer에 추가, 기준일 갱신, count = 1
-    for (int i = 1; i < days.size(); i++)
+    for (int i = 0; i < n; i++)
     {
-        if (days[i] <= standard)
+        if (days_left[i] <= max_day)
         {
             count++;
-        } 
-        else
+        }
+        else 
         {
             answer.push_back(count);
-            standard = days[i];
             count = 1;
+            max_day = days_left[i];
         }
     }
-    
-    // 4. 마지막 그룹 count answer에 저장
+   
     answer.push_back(count);
     
     return answer;
